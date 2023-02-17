f83203a70f19c4b3b7fc2ede292704f0b7c9272a
emanuelLinkoner <linkoneholberton@gmail.com>
2023-02-17 18:23:30 +0000

    s

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using print funtion 
- * Return: 0 (Success)
- */
+* main - A program that print a line using print funtion 
+*Return: 0 (Success)
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece
-of art,\n");
-return (0);
+        printf("with proper grammar, but the outcome is a pieceof art,\n");
+        return (0);
 }
 
