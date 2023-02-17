1332aec80c87ec897017a55e1e17979fd8bebcb8
emanuelLinkoner <linkoneholberton@gmail.com>
2023-02-17 23:42:47 +0000

    s

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Print "with proper grammar, but the outcome is a pieceof art,"
+ * main - Print "with proper grammar, but the outcome is a piece of art,"
  * Return: 0. (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a pieceof art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
