8383076b7d28789e210768bee251bf84d3cd07c5
emanuelLinkoner <linkoneholberton@gmail.com>
2023-02-21 23:57:08 +0000

    s

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdiio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
