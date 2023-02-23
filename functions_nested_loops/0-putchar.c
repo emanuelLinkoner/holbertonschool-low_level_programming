cd7b6a2c269a9736ff2e7ffbb16ecdcae10928cb
emanuelLinkoner <linkoneholberton@gmail.com>
2023-02-23 14:48:56 +0000

    s

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point 
+ * main - Entry point
  * int ch - introduce ch
  * for = for every ch <= print body
  * Return: always 0
- */
+ * /
 
 int main(void)
 {
