e6d296d99523e45cf9ee22eac055b0ef5c372adc
emanuelLinkoner <linkoneholberton@gmail.com>
2023-02-26 16:44:53 +0000

    is super

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+// C program to demonstrate
+// isupper() function
+#include <ctype.h>
+#include <stdio.h>
+int main()
+{
+    char ch = 'c';
+ 
+    // checking uppercase
+    if (isupper(ch))
+        printf(1);
+    else
+        printf(0);
+}
