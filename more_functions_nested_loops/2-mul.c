2fdb4588b81e267fa0812b2fbf81490921644314
emanuelLinkoner <linkoneholberton@gmail.com>
2023-02-27 21:38:27 +0000

    s

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,6 +4,7 @@
  * mul - Multiples two integers
  * @x: The first number to multiply
  * @y: The second number to multyply with first number
+ * Return: El resultado de la multiplicacion
  */
 
 int mul(int x, int y)
