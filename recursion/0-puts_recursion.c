5b8fb2319e41cb1582e1177bd5ddd32019668d09
emanuelLinkoner <linkoneholberton@gmail.com>
2023-03-07 15:09:17 +0000

    void _puts_recursion(char *s);

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,9 +3,9 @@
 
 void _puts_recursion(char *s){
 
-	if (*s == "/0")
+	if (*s == '/0')
 	{
-		_putchar("/n')
+		_putchar('/n')
 		return;
 	}
 
@@ -13,4 +13,3 @@ void _puts_recursion(char *s){
 	s++;
 	_puts_recursion(s);
 }
-
