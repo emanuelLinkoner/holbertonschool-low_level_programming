8529cfa0654a1c52918c0d9013199af7d0dc221a
emanuelLinkoner <linkoneholberton@gmail.com>
2023-03-10 20:55:58 +0000

    s

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - Prints n elements of an array of integers
@@ -13,11 +14,11 @@ void print_array(int *a, int n)
 
 	for (j = 0; j < n; j++)
 	{
-		printf("%d", a[j]);
-		if (j != (n - 1))
-		{
-		printf(",");
-		}
+	printf("%d", a[j]);
+	if (j != (n - 1))
+	{
+	printf(",");
+	}
 	}
 	printf("\n");
 }
