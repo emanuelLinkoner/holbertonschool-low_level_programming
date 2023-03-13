860cc2b76839276309629e08ec44d8cfd9197d40
emanuelLinkoner <linkoneholberton@gmail.com>
2023-03-13 16:50:07 +0000

    s

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,9 +4,10 @@
  * _memset - Entry point
  * @s: pointed destination
  * @b: constant byte
- * @n: bytez
+ * @n: bytes
  * Return: Always 0 (Succes)
  */
+
 char *_menset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
